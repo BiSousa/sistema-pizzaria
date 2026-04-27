c91edefbdbc4e98e447b6ff733f8652577fc8522
BiSousa <biacsousa72@gmail.com>
2026-04-27 14:52:17 -0300

    feat: add data requisitions

--- src/main/java/com/pizzaria/sistema/repository/IngredienteRepository.java
+++ src/main/java/com/pizzaria/sistema/repository/IngredienteRepository.java
@@ -2,6 +2,10 @@ package com.pizzaria.sistema.repository;
 
 import com.pizzaria.sistema.model.Ingrediente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface IngredienteRepository extends JpaRepository<Ingrediente, Long> {
+    Optional<Ingrediente> findByNome(String nome);
+    boolean existsByNome(String nome);
 }
