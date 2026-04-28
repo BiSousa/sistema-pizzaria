9637645b25377337ea0644d3d27ccd3eb792b458
BiSousa <biacsousa72@gmail.com>
2026-04-28 16:48:50 -0300

    refactor: update repository, service and controller with validations

--- src/main/java/com/pizzaria/sistema/repository/CategoriaRepository.java
+++ src/main/java/com/pizzaria/sistema/repository/CategoriaRepository.java
@@ -3,5 +3,9 @@ package com.pizzaria.sistema.repository;
 import com.pizzaria.sistema.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByDescricao(String descricao);
+    boolean existsByDescricao(String descricao);
 }
